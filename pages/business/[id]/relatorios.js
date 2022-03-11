ab005f8247bf77be6b789ad4fd909abcb1c9a849
LeoSantini <95887191+LeoSantini@users.noreply.github.com>
2022-03-10 21:01:43 -0300

    Includes new dash

--- pages/business/[id]/relatorios.js
+++ pages/business/[id]/relatorios.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
 import Chart from "../../../components/Chart";
 import NavbarBusiness from "../../../components/NavbarBusiness";
 
 function Report() {
+  // const business = localStorage.getItem("loggedInBusiness");
+  // console.log(business);
+  // const [filterBusiness, setFilterBusiness] = useState({});
+  // const [id, setId] = useState();
+
+  // useEffect(() => {
+  //   setId(localStorage.getItem("loggedInBusiness"));
+  //   const parsedBusiness = JSON.parse(id || '""');
+  //   setFilterBusiness({ business: parsedBusiness });
+  // }, [id]);
+
+  // console.log(filterBusiness);
+
   return (
     <>
       <NavbarBusiness />
@@ -10,7 +24,7 @@ function Report() {
         <Chart
           height={"410px"}
           width={"559px"}
-          chartId={"62289d58-5f2f-4297-8114-4374b1c85f8d"}
+          chartId={"622a7e59-3d53-43cb-8393-14f3e5eb28bb"}
           filter={""}
         />
         <Chart
@@ -37,6 +51,20 @@ function Report() {
           height={"410px"}
           width={"369px"}
           chartId={"622a3694-7bb4-490a-82e5-9ea31197af58"}
+          filter={""}
+        />
+      </div>
+      <div>
+        <Chart
+          height={"410px"}
+          width={"938px"}
+          chartId={"622a73bf-efdd-4f9b-8eb2-37dce878d992"}
+          filter={""}
+        />
+        <Chart
+          height={"410px"}
+          width={"938px"}
+          chartId={"e95ccddf-4581-40b8-b13e-f4373bebe685"}
           filter={""}
         />
       </div>
